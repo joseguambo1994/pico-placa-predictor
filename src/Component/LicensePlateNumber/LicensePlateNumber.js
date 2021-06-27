a075d95cd05649c71119b82412bf1b64cb935788
jlguambo <jlguambo@gmail.com>
2021-06-27 14:53:17 -0500

    PPP-4 - Add input regex validation to LicensePlateNumber component.

--- src/Component/LicensePlateNumber/LicensePlateNumber.js
+++ src/Component/LicensePlateNumber/LicensePlateNumber.js
@@ -3,16 +3,28 @@ import { useState } from 'react';
 const LicensePlateNumber = ({onLicensePlateChange}) => {
 
     const [licensePlate,setLicensePlate] = useState("AAA-HG3");
+    const [count, setCount] = useState(0);
+    const carLettersValidation = new RegExp('^[A-Z]{3}[0-9]{3}$');
     
     function handleChange(e){
-        onLicensePlateChange(e.target.value);
-        setLicensePlate(e.target.value);
+        var word = "" + e.target.value;
+        word = word.toUpperCase();
+        if(carLettersValidation.test(word)){
+            setLicensePlate(word.toUpperCase());
+            onLicensePlateChange(word);
+        }else{
+            setLicensePlate("Invalid");
+        }   
+        setCount(count + 1);
+        
     }
 
     return (
     <div>
-      <input type="Text" name="LicensePlateNumber" onChange={ handleChange }/>
+      <input type="Text" name="LicensePlateNumber" onChange={ handleChange }
+       style={{textTransform: "uppercase"}} maxLength="6"/>
       <p> The plate number is: {licensePlate}</p>
+      <p> {count}</p>
     </div>
   );
 };
